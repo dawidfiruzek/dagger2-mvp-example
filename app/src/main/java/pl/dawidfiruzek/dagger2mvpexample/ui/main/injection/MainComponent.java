d160743d2356ed38364608f7c374df1d269a2077
Dawid Firuzek <dawid.firuzek@gmail.com>
2016-09-25 21:42:08 +0200

    Removed @Singleton annotation from main component

--- app/src/main/java/pl/dawidfiruzek/dagger2mvpexample/ui/main/injection/MainComponent.java
+++ app/src/main/java/pl/dawidfiruzek/dagger2mvpexample/ui/main/injection/MainComponent.java
@@ -1,15 +1,12 @@
 package pl.dawidfiruzek.dagger2mvpexample.ui.main.injection;
 
-import javax.inject.Singleton;
-
 import dagger.Component;
-import pl.dawidfiruzek.dagger2mvpexample.util.injection.AppComponent;
 import pl.dawidfiruzek.dagger2mvpexample.ui.main.MainActivity;
+import pl.dawidfiruzek.dagger2mvpexample.util.injection.AppComponent;
 
 /**
  * Created by Dawid Firuzek on 12.08.16.
  */
-@Singleton
 @Component(
         dependencies = AppComponent.class,
         modules = MainModule.class
